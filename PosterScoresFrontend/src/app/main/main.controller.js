dacea17418d3a478d8540c50bfb1ddd9fa0361a5
Aaron Abbott <aabmass@gmail.com>
2015-11-10 00:46:41 -0500

    Basic working social log in!
    At this stage, the example seems to work just fine! The only issue is
    that, even though it 'email' is requested in the scope for both the
    front and back ends, the email doesn't seem to get registered in django

--- PosterScoresFrontend/src/app/main/main.controller.js
+++ PosterScoresFrontend/src/app/main/main.controller.js
@@ -6,7 +6,25 @@
     .controller('MainController', MainController);
 
     /** @ngInject */
-    function MainController() {
+    function MainController($auth, $log) {
+        var vm = this;
+
+        vm.loggedIn = $auth.isAuthenticated();
+        vm.user = {};
+
+        $log.log($auth.getPayload());
+
+        vm.authenticate = function(provider) {
+            $auth.authenticate(provider, {
+                provider: provider
+            }).then(function(response) {
+                vm.loggedIn = true;
+                
+                $log.log(response.data.first_name);
+                vm.user.first = response.data.first_name;
+                vm.user.last = response.data.last_name;
+            });
+        };
 
     }
 })();
